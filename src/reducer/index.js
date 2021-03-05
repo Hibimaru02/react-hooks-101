13a9681e667e7cc658e71ae2e72e2d97cc68e99d
Hibimaru <info@hibimaru.com>
2021-03-05 20:11:37 +0900

    implement event creation

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,53 +1,55 @@
-// action = {
-//    type: 'CREATE_EVENT'
-//    title: '2020東京オリンピック'
-//    body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、、、'
-// }
-// before
-// state = []
 
-// #after
+// action = {
+//   type: 'CREATE_EVENT',
+//   titile: '2020東京オリンピックのお知らせ',
+//   body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、'
+// }
+//
+//
+// # before
+// state = []
+//
+// # after
 // state = [
-//    {
+//   {
 //     id: 1,
-//     title: '2020東京オリンピック',
-//     body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、、、'
-//    }
+//     titile: '2020東京オリンピックのお知らせ',
+//     body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、'
+//   }
 // ]
 //
-// #before
+// # before
 // state = [
-//    {id: 1, title: 'タイトル1', body: 'ボディー1'},
-//    {id: 2, title: 'タイトル2', body: 'ボディー2'},
-//    {id: 3, title: 'タイトル3', body: 'ボディー3'}
+//   { id: 1, title: 'タイトル1', body: 'ボディー1'},
+//   { id: 2, title: 'タイトル2', body: 'ボディー2'},
+//   { id: 3, title: 'タイトル3', body: 'ボディー3'},
 // ]
 //
-// #after
+// # after
 // state = [
-//    {id: 1, title: 'タイトル1', body: 'ボディー1'},
-//    {id: 2, title: 'タイトル2', body: 'ボディー2'},
-//    {id: 3, title: 'タイトル3', body: 'ボディー3'},
-//    {
+//   { id: 1, title: 'タイトル1', body: 'ボディー1'},
+//   { id: 2, title: 'タイトル2', body: 'ボディー2'},
+//   { id: 3, title: 'タイトル3', body: 'ボディー3'},
+//   {
 //     id: 4,
-//     title: '2020東京オリンピック',
-//     body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、、、'
-//    }
-// ]  
-//
+//     titile: '2020東京オリンピックのお知らせ',
+//     body: '2020年に東京でオリンピックを開催します！つきましては、、、、、、、'
+//   }
+// ]
 const events = (state = [], action) => {
     switch(action.type) {
-        case 'CREATE_EVENT':
-            const event = { title: action.title, body: action.body }
-            const length = state.length
-            const id = lenth === 0 ? 1 : state[length -1] + 1
-            return [...state, { id, ...event }]
-        case 'DALETE_EVENT':
-            return state
-        case 'DARETE_ALL_EVENTS':
-            return []
-        default:
-            return state
+      case 'CREATE_EVENT':
+        const event = { title: action.title, body: action.body }
+        const length = state.length
+        const id = length === 0 ? 1 : state[length - 1].id + 1
+        return [...state, { id, ...event }]
+      case 'DELETE_EVENT':
+        return state
+      case 'DELETE_ALL_EVENTS':
+        return []
+      default:
+        return state
     }
-}
-
-export default events
+  }
+  
+  export default events
